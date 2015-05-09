32b68a11ca4f2f810d78a4af8ba63d46e240e9c7
Morvana <morvanabonin@gmail.com>
2015-05-08 23:28:19 -0300

    files of js tests

---
+++ fibonacci_ex.js
@@ -0,0 +1,40 @@
+/**
+class Fibonacci() {
+
+    constructor() {}
+
+    fibo(n) {
+        return (n < 2) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+}
+let n;
+let fibonacci = new Fibonacci();
+console.log(fibonacci.fibo(8));
+*/
+
+function fibonacci(n) {
+
+    return (n < 2) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+
+}
+let n;
+console.log(fibonacci(8));
+
+console.log(fibonacci(8));
+
+function fibonacci(n) {
+    let previous = 0;
+    let current = 1;
+
+    for(let i = 0; i < n; i += 1) {
+        let temp = previous;
+        previous = current;
+        current = temp + current;
+    }
+
+    return current;
+
+}
+
+console.log(fibonacci(8));
---
+++ test.js
@@ -0,0 +1,2 @@
+let a = "I'm a test of version and compatibility.";
+console.log(a);
