07143e1a04b344cc85c9d0688660e8d0c1c49379
Morvana <morvanabonin@gmail.com>
2015-05-10 22:25:29 -0300

    More example/tests with methods im JS.

--- src/method_example.js
+++ src/method_example.js
@@ -13,6 +13,23 @@ Cat.prototype.meow = function() {
 
 var cat1 = new Cat("Kaka", "white");
 var cat2 = new Cat("Keko", "black");
+var catFunction = cat1.scratch;
 
+// logs "Kaka, the white cat, scratch the sofa"
 cat1.scratch();
+
+// logs "The black cat, called Keko, meowed at the moon."
 cat2.meow();
+
+// logs ", the undefined cat, scratch the sofa" (or fails
+// with a TypeError in strict mode)
+catFunction();
+
+//logs true
+console.log(catFunction === cat1.scratch);
+
+//logs true
+console.log(catFunction === Cat.prototype.scratch);
+
+//logs "Kaka, the white cat, scratch the sofa"
+catFunction.call(cat1);
